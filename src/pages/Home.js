b7cfe4c1c51ce11b7fd22aa86fdda3fc820097f2
Karlcamp162 <ckarlcamp@gmail.com>
2025-06-27 18:14:44 +0800

    update commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const cloudWidth = 300; // Adjust to match your cloud image width in px
+
+// Reusable CloudMarquee component
+const CloudMarquee = ({ top, height, duration, direction = 'right' }) => {
+  // Determine initial and animate x values based on direction
+  const initialX = direction === 'right' ? -cloudWidth : '100vw';
+  const animateX = direction === 'right' ? '100vw' : -cloudWidth;
+  return (
+    <motion.img
+      src="/cloud.png"
+      alt="Moving Cloud"
+      className={`fixed z-[-1] m-1 ${height}`}
+      style={{ top, left: 0, position: 'fixed' }}
+      initial={{ x: initialX }}
+      animate={{ x: animateX }}
+      transition={{
+        repeat: Infinity,
+        repeatType: 'loop',
+        duration,
+        ease: 'linear'
+      }}
+    />
+  );
+};
 
 const Home = () => {
   return (
@@ -10,8 +36,8 @@ const Home = () => {
         src="/road.png"
         className="fixed bottom-0 w-full object-cover z-[-1] h-[60px] sm:h-[80px] lg:h-[100px]"
         alt="road_img"
-      />
-
+      />  
+      
       <div className="flex flex-col items-center justify-center animate-fade-in px-4 sm:px-6 lg:px-8 mt-4 sm:mt-8 lg:mt-6 mb-20">
         <div className="text-center">
           <p className="text-5xl sm:text-7xl lg:text-9xl font-bold text-yellow-300 leading-tight">
@@ -46,9 +72,16 @@ const Home = () => {
       </div>
 
       {/* eslint-disable-next-line jsx-a11y/no-distracting-elements */}
-      <marquee behavior="alternate" direction="right" scrollAmount="8" className="fixed bottom-[35px] sm:bottom-[45px] lg:bottom-[60px] z-[-1]">
+      <marquee behavior="alternate" direction="right" scrollAmount="3" className="fixed bottom-[35px] sm:bottom-[45px] lg:bottom-[60px] z-[-1]">
         <img src="/mob.png" alt="Moving Mob" className="h-[30px] sm:h-[40px] lg:h-[50px] m-1"/>
       </marquee>
+
+      {/* Cloud 1 - Top */}
+      <CloudMarquee top="35px" height="h-[70px] sm:h-[90px] lg:h-[120px]" duration={48} direction="right" />
+      {/* Cloud 2 - Middle */}
+      <CloudMarquee top="35%" height="h-[90px] sm:h-[110px] lg:h-[140px]" duration={58} direction="right" />
+      {/* Cloud 3 - Bottom */}
+      <CloudMarquee top="65%" height="h-[80px] sm:h-[100px] lg:h-[130px]" duration={52} direction="left" />
     </>
   );
 };
